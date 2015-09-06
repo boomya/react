be70ee12fb4ad8acad6d2fb493b8bf2389cfb8f8
coder.jiangshan <j_shan1984@126.com>
2015-09-06 14:37:04 +0800

    add 6to5

---
+++ getting-started/react_qa_es6/app/js/main.js
@@ -0,0 +1,4 @@
+var React = require('react');
+var QuestionApp = require('./component/QuestionApp.js');
+
+React.render(<QuestionApp />, document.getElementById('app'));
---
+++ getting-started/react_qa_es6/gulpfile.js
@@ -0,0 +1,60 @@
+var gulp = require('gulp'),
+    connect = require('gulp-connect'),
+    // browserify = require('gulp-browserify'),
+    // concat = require('gulp-concat'),
+    browserify = require('browserify'),
+    babelify = require('babelify'),
+    source = require('vinyl-source-stream'),
+    port = process.env.port || 5000;
+
+gulp.task('browserify', function () {
+    browserify({
+        entries: './app/js/main.js',
+        debug: true
+    })
+    .transform(babelify)
+    .bundle()
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest('./dist'));
+
+});
+
+// gulp.task('browserify', function () {
+//     gulp.src('./app/js/main.js')
+//         .pipe(browserify({
+//             transform: 'reactify',
+//         }))
+//         .pipe(gulp.dest('./dist/js'))
+// });
+
+//live reload
+gulp.task('connect', function () {
+    connect.server({
+        root: './',
+        port: port,
+        livereload: true,
+    })
+});
+
+//reload js
+gulp.task('js', function () {
+    gulp.src('./js/**/*.js')
+        .pipe(connect.reload())
+});
+
+//reload html
+gulp.task('html', function () {
+    gulp.src('./*.html')
+        .pipe(connect.reload())
+});
+
+//watch trigger
+gulp.task('watch', function () {
+    gulp.watch('./dist/**/*.js', ['js']);
+    gulp.watch('./*.html', ['html']);
+    gulp.watch('./app/js/**/*.js', ['browserify']);
+});
+
+gulp.task('default', ['browserify']);
+
+gulp.task('serve', ['browserify', 'connect', 'watch']);
